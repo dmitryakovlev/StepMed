88fc73d870a1d7f09d0ac6865180b999b6d5e4f9
Yuri Yakovlev <hi@mynameisyuri.com>
2021-04-21 00:19:43 +0200

    Add sendTelegramMessage endpoint

---
+++ pages/api/sendTelegramMessage.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  status: string;
+};
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    const messageText = req.body.messageText
+      ? `&text=${encodeURIComponent(req.body.messageText)}`
+      : '';
+
+    const sentMessage = await fetch(
+      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}}${messageText}`,
+    );
+
+    if (sentMessage.status >= 400) {
+      throw new Error();
+    }
+
+    res.status(200).json({ status: 'Message sent.' });
+  } catch (error) {
+    res.status(400).json({ status: "Message wasn't sent." });
+  }
+};
