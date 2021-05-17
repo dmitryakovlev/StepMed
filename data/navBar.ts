60efe1a1aa4b2c82aabca658452446b55ceff03f
Dmitriy Yakovlev <yakovlev.dragomir@gmail.com>
2021-05-18 01:07:14 +0200

    New page

--- data/navBar.ts
+++ data/navBar.ts
@@ -114,11 +114,11 @@ export const forPatientsSubMenu: INavBarMenu[] = [
     url: '/pravila-gospitalizacii',
     title: 'Правила госпитализации',
   },
-  // {
-  //   id: 8,
-  //   url: '/podgotovka-k-issledovaniyam',
-  //   title: 'Подготовка к исследованиям',
-  // },
+  {
+    id: 8,
+    url: '/podgotovka-k-issledovaniyam',
+    title: 'Подготовка к исследованиям',
+  },
   {
     id: 9,
     url: '/anketa-kachestva',
